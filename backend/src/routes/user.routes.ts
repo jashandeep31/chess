d981a018935e54062977b429cfc51e3ec1ca21a4
jashandeep <jashandeep1659@gmail.com>
2024-09-14 15:48:56 +0530

    feat: add the profile api

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,4 +4,23 @@ import { userLoginHandler } from "../controllers/user.controllers";
 const routes = Router();
 
 routes.route("/login").get(userLoginHandler);
+
+routes.route("/profile").get((req, res) => {
+  const user: any = req.user;
+  console.log(user);
+  console.log(req.isAuthenticated());
+  if (!req.isAuthenticated() || !user) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  return res.status(200).json({
+    session: {
+      id: user.id,
+      email: user.emails[0].value,
+      name: user.displayName,
+      avatar: user.photos[0].value,
+    },
+  });
+});
 export default routes;
